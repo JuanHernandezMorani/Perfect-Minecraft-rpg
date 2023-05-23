6d6e577bb46520709ce968416ce9c802aa2589d5
JuanHernandezMorani <juan.hernandez.morani@gmail.com>
2023-05-23 18:11:06 -0300

    first commit

---
+++ src/main/java/net/cheto97/rpgcraftmod/RpgcraftMod.java
@@ -0,0 +1,35 @@
+package net.cheto97.rpgcraftmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(RpgcraftMod.MOD_ID)
+public class RpgcraftMod{
+    public static final String MOD_ID = "rpgcraftmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public RpgcraftMod(){
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents{
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+}
