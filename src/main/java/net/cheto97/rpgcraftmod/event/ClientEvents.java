87dd1d2d2173f7224e83979c8bd824975ed9038e
JuanHernandezMorani <juan.hernandez.morani@gmail.com>
2023-05-24 21:42:51 -0300

    Added keymapping system

---
+++ src/main/java/net/cheto97/rpgcraftmod/event/ClientEvents.java
@@ -0,0 +1,33 @@
+package net.cheto97.rpgcraftmod.event;
+
+import net.cheto97.rpgcraftmod.RpgcraftMod;
+import net.cheto97.rpgcraftmod.util.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = RpgcraftMod.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents{
+
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event){
+            if(KeyBinding.DRINKING_KEY.consumeClick()){
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Key pressed!"));
+            }
+
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = RpgcraftMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event){
+            event.register(KeyBinding.DRINKING_KEY);
+        }
+    }
+}
---
+++ src/main/java/net/cheto97/rpgcraftmod/util/KeyBinding.java
@@ -0,0 +1,14 @@
+package net.cheto97.rpgcraftmod.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinding {
+    public static final String KEY_CATEGORY_RGPCRAFT = "key.category.rpgcraftmod.rpgcraft";
+    public static final String KEY_DRINK_MANA = "key.rpgcraftmod.drink_mana";
+
+    public static final  KeyMapping DRINKING_KEY = new KeyMapping(KEY_DRINK_MANA, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O, KEY_CATEGORY_RGPCRAFT);
+
+}
